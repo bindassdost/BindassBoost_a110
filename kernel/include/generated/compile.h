c1594c99d3148a3648287e85f03023b60c51662c
bindassdost <bindassdost@gmail.com>
2014-08-04 01:26:16 +0530

    selinux config + binder patch

--- kernel/include/generated/compile.h
+++ kernel/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 138 */
+/* This file is auto generated, version 139 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#138 SMP PREEMPT Fri May 30 10:24:00 IST 2014"
+#define UTS_VERSION "#139 SMP PREEMPT Wed Jun 4 15:47:49 IST 2014"
 #define LINUX_COMPILE_BY "BindassDost"
 #define LINUX_COMPILE_HOST "BindassStudio"
 #define LINUX_COMPILER "gcc version 4.8.3 20140303 prerelease (crosstool-NG linaro-1.13.1+bzr2647 - Linaro GCC 2014.03 - Cortex-A9) "
